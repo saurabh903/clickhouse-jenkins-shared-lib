7bb3c799b1b95fedc14f80db235e79c0bffc5a44
Saurabh Prasad 💪 <saurabhprasad903@gmail.com>
2025-08-01 00:01:49 +0530

    Update Utils.groovy

--- src/org/example/Utils.groovy
+++ src/org/example/Utils.groovy
@@ -1,1 +1,15 @@
+package org.example
+
+class Utils implements Serializable {
+    def steps
+
+    Utils(steps) {
+        this.steps = steps
+    }
+
+    def loadConfig(String path) {
+        def props = steps.readProperties(file: path)
+        return props
+    }
+}
 
