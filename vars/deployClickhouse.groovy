87e85b3df0d2a337e011ae7d9f4f273d0597a3be
Saurabh Prasad 💪 <saurabhprasad903@gmail.com>
2025-08-01 00:02:20 +0530

    Update deployClickhouse.groovy

--- vars/deployClickhouse.groovy
+++ vars/deployClickhouse.groovy
@@ -1,1 +1,25 @@
+def call(String configPath = "resources/deployClickhouse/config.properties") {
+    def utils = new org.example.Utils(this)
+    def config = utils.loadConfig(configPath)
+
+    stage('Clone Repo') {
+        checkout scm
+    }
+
+    if (config.KEEP_APPROVAL_STAGE?.toBoolean()) {
+        stage('User Approval') {
+            input message: "Do you want to proceed with ${config.ENVIRONMENT} deployment?", ok: "Yes"
+        }
+    }
+
+    stage('Run Ansible Playbook') {
+        sh """
+            ansible-playbook ${config.CODE_BASE_PATH}/clickhouse-deploy.yml -e env=${config.ENVIRONMENT}
+        """
+    }
+
+    stage('Notify') {
+        slackSend(channel: "#${config.SLACK_CHANNEL_NAME}", message: "${config.ACTION_MESSAGE}")
+    }
+}
 
